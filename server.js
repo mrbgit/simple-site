306e475ff9eccf21f1d1d68fe23c0523a34cf00f
Michael Bowen <mikee.bowen@gmail.com>
2015-03-16 00:56:39 -0700

    move tutnese function to its own file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyparser = require("body-parser");
 var port = process.env.PORT || 3000;
 var piglatinify = require("./lib/piglatinify.js");
-// var tutneseify = require("./lib/tutnese.js");
+var tutneseify = require("./lib/tutnese.js");
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
@@ -86,42 +86,6 @@ app.post("/piglatin", function(req, res) {
   lastname};
   res.json(piglatined);
 });
-
-  // Tutnese function
-function tutneseify(inputName) {
-  // create needed variables
-  var individualLetters;
-  var newName;
-  // make list of vowels
-  var vowels = ["a","e","i","o","u"];
-  // create object for consonants
-  var tutneseConsonants = {b: ["bub", "bab"], c: ["cash", "coch"], d: ["dud", "dorl"], f: ["fuf", "fud"], g: ["gug", "gul"], h: ["hash", "hutch"], j: ["jay", "jug"], k: ["kuck", "klack"], l: ["lul", "lon"], m: ["mum", "mom"], n: ["nun", "nog"], p: ["pub", "pup"], q: ["quack", "queue"], r: ["rug", "rur"], s: ["sus", "sag"], t: ["tut", "taf"], v: ["vuv", "van"], w: ["wack", "wash"], x: ["ex", "xux"], y: ["yub", "yuck"], z: ["zub", "zug"]};
-  // make letters lowercast
-  inputName.toLowerCase();
-  // separate letters of original word into array of letters
-  var separatedLetters = inputName.split("");
-  // for loop to go through array of letters
-  for (var i = 0; i < separatedLetters.length; i++) {
-      var currentItem = separatedLetters[i];
-    // if the letter in the arrray is not a vowel
-    if (vowels.indexOf(separatedLetters[i]) !== -1) {} else {
-      // get a random number between 0 and 1
-      var randomNum = Math.round(Math.random());
-      // make the value of the letter equal to a random item in the array
-      separatedLetters[i] = tutneseConsonants[currentItem][randomNum];
-      }
-  }
-  // join array into string
-  newName = separatedLetters.join("");
-  // split string into array of individual letters
-  newName = newName.split("");
-  // set the first letter in array to uppercase
-  newName[0] = newName[0].toUpperCase();
-  // join array back into string
-  newName = newName.join("");
-  // return value of function
-  return newName;
-}
 
 app.post("/tutnese", function(req, res) {
   var firstnameTutnese = tutneseify(req.body.firstnameTutnese);
