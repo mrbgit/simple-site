c4dd19428368f5bf706ce0f7726aca7b5e67df9c
Michael Bowen <mikee.bowen@gmail.com>
2015-03-16 13:48:35 -0700

    remove extra alphabet array

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ var team = [{ name: "Osvaldo Alonso",
               position: "Forward"},
               {name: "Obafemi Martins",
               position: "Forward"}];
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x','y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&'];
 var randomString = '';
 var jokes = [
 {setup: 'What do you call a masturbating cow?',
