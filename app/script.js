7bfecd4c796438464c2db1c9f75aaed81bca75a7
Michael Bowen <mikee.bowen@gmail.com>
2015-03-16 22:27:22 -0700

    clean up style with grunt

--- app/script.js
+++ app/script.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-  $("#myButton").on("click", function(){
-    $.get("randomStringFromArray", function (response) {
+  $("#myButton").on("click", function() {
+    $.get("randomStringFromArray", function(response) {
     $("#answer").text(response);
     });
-  })
+  });
 
   $("#jokes").on("click", function() {
-    $.get("jokes", function (response) {
+    $.get("jokes", function(response) {
       $("#ajax-text").text(response);
-    })
+    });
   });
 
   $("#soccerButton").on("click", function() {
-    $.get("pickATeam", function (response) {
+    $.get("pickATeam", function(response) {
       $("#soccerTeam").text(response);
-    })
+    });
   });
 
   $("#piglatin").on("submit", function(e) {
     e.preventDefault();
-    var firstname = $('input[name=firstname]').val();
-    var lastname = $('input[name=lastname]').val();
-    var name = {firstname: firstname, lastname: lastname};
-    $.post("piglatin", name, function (response) {
+    var firstname = $("input[name=firstname]").val();
+    var lastname = $("input[name=lastname]").val();
+    var name = { firstname: firstname, lastname: lastname };
+    $.post("piglatin", name, function(response) {
       var piglatinified = response.firstname + " " +
       response.lastname;
     $("#piglatinified").text(piglatinified);
@@ -37,7 +37,7 @@ $(document).ready(function() {
     // get the values from the tutnese form
     var firstnameTutnese = $("input[name=firstnameTutnese]").val();
     var lastnameTutnese = $("input[name=lastnameTutnese]").val();
-    var tutneseName = {firstnameTutnese: firstnameTutnese, lastnameTutnese: lastnameTutnese};
+    var tutneseName = { firstnameTutnese: firstnameTutnese, lastnameTutnese: lastnameTutnese };
     $.post("tutnese", tutneseName, function(response) {
       var madeIntoTutanese = response.firstnameTutnese + " " +
       response.lastnameTutnese;
--- server.js
+++ server.js
@@ -6,23 +6,22 @@ var piglatinify = require("./lib/piglatinify.js");
 var tutneseify = require("./lib/tutnese.js");
 var randomStringFromArray  = require("./lib/random-string.js");
 var randomJoke  = require("./lib/jokes.js");
-var checkPlayer = require("./lib/sounders.js");
-var pickATeam = require("./lib/mls.js")
+var pickATeam = require("./lib/mls.js");
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
 // use /app as home folder
-app.use(express.static(__dirname + '/app/'));
+app.use(express.static(__dirname + "/app/"));
 // listen for the server port and console log it
-app.listen(port, function(){
-  console.log('Server has been started on ' + port);
+app.listen(port, function() {
+  console.log("Server has been started on " + port);
 });
 //make index.html home page
-app.get('/', function(req, res){
-  res.sendFile('index.html');
+app.get("/", function(req, res) {
+  res.sendFile("index.html");
 });
 // serve random joke from json object
-app.get('/jokes', function(req, res){
+app.get("/jokes", function(req, res) {
 // run the joke function and use json to assign the value to the res parameter
   res.json(randomJoke());
 });
@@ -30,23 +29,23 @@ app.get('/jokes', function(req, res){
 app.get("/randomStringFromArray", function(req, res) {
   // run the random string function and use json to assign the value to the res parameter
   res.json(randomStringFromArray());
-})
+});
 // run the random team function and use json t assign the value to the res parameter
-app.get("/pickATeam", function (req, res) {
+app.get("/pickATeam", function(req, res) {
   res.json(pickATeam());
-})
+});
 // take values from pig latin from from and use them as arguments for the pig lating function, then assign the result of the function to the res parameter with json
 app.post("/piglatin", function(req, res) {
   var firstname = piglatinify(req.body.firstname);
   var lastname = piglatinify(req.body.lastname);
-  var piglatined = {firstname: firstname, lastname:
-  lastname};
+  var piglatined = { firstname: firstname, lastname:
+  lastname };
   res.json(piglatined);
 });
 // take values from tutnese from from and use them as arguments for the tutnese function, then assign the result of the function to the res parameter with json
 app.post("/tutnese", function(req, res) {
   var firstnameTutnese = tutneseify(req.body.firstnameTutnese);
   var lastnameTutnese = tutneseify(req.body.lastnameTutnese);
-  var tutnesed = {firstnameTutnese: firstnameTutnese, lastnameTutnese: lastnameTutnese};
+  var tutnesed = { firstnameTutnese: firstnameTutnese, lastnameTutnese: lastnameTutnese };
   res.json(tutnesed);
 });
